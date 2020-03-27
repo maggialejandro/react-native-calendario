fa9cda645701578fa7f8b68a803dab2d939ce096
周刘通 <zltong>
2020-03-27 15:40:04 +0800

    add: add chinese language support

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -83,6 +83,21 @@ export function getMonthNames(locale?: LocaleType): string[] {
         'Novembro',
         'Dezembro',
       ];
+    case 'zh':
+      return [
+        '一月',
+        '二月',
+        '三月',
+        '四月',
+        '五月',
+        '六月',
+        '七月',
+        '八月',
+        '九月',
+        '十月',
+        '十一月',
+        '十二月',
+      ];
     default:
       return [
         'January',
